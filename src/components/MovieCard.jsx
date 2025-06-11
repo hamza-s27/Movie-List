9151daaab02ad8b6aaa5f54ee77b01122575ae78
Hamza Shirazi <hamzashirazi@Hamzas-MacBook-Pro.local>
2025-06-11 15:04:18 +0500

    css intigration and api file made

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,3 +1,5 @@
+import "../css/MovieCard.css"
+
 
 function onFavClick(){
     alert ("clicked")
--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,3 +1,5 @@
+import "../css/Favorites.css";
+
 function Favorites(){
     return <div className="favorites-empty">
         <h2>No Favorite movies yet</h2>
